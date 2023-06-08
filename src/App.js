7a2fd9f573d8cee0aff08d0103e2d33c9e093183
forLoop94 <charlesuwem94@gmail.com>
2023-06-08 11:40:28 +0100

    Setup up router and add routes to the application

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hi React</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<h1>Hello React</h1>} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
   );
 }
 
