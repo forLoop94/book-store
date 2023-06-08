2237f65af8609ec28ba0b7b8eb4aac2afa2304cd
forLoop94 <charlesuwem94@gmail.com>
2023-06-08 12:59:53 +0100

    Add temporary styling to separate book objects

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
+import style from '../styles/BookList.module.css';
 
 const BookItem = ({ book }) => (
-  <>
+  <li className={style.book}>
     <small>{book.category}</small>
     <h3>{book.title}</h3>
     <p>{book.author}</p>
-    <div className="flex btn-container">
+    <div className={style.btnContainer}>
       <button type="button">Comments</button>
       <button type="button">Remove</button>
       <button type="button">Edit</button>
     </div>
-  </>
+  </li>
 );
 
 BookItem.propTypes = {
