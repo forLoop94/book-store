dd22c97275e1ada245fa83a5a8cde03f43021e84
forLoop94 <charlesuwem94@gmail.com>
2023-06-13 10:25:39 +0100

    Define a slice of state for categories that includes a reducer that checks the status and always returns Under Construction

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -2,11 +2,15 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   categories: [],
+  status: 'Under construction',
 };
 
 const categoriesSlice = createSlice({
   name: 'categories',
   initialState,
+  reducers: {
+    checkStatus: () => initialState,
+  },
 });
 
 export const { checkStatus } = categoriesSlice.actions;
