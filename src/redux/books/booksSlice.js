9a72325bb1b0cf475871d75d92c6b2da7166400f
forLoop94 <charlesuwem94@gmail.com>
2023-06-15 13:19:46 +0100

    Make HTTP request using axios and createAsyncThunk and add functions to create and delete posts in the server

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,4 +1,5 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
   bookList: [
@@ -22,6 +23,38 @@ const initialState = {
     },
   ],
 };
+
+const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/6PniCavOBYZV6AbPZ91l/books';
+
+export const fetchBook = createAsyncThunk('books/fetchBooks', () => {
+  try {
+    const data = await axios.get(url);
+    return data.data;
+  } catch (error) {
+    return error;
+  }
+});
+
+export const createBook = createAsyncThunk('books/createBook', async (book, thunk) => {
+  const res = JSON.stringify(book);
+  try {
+    await axios.post(url, res, { header: { 'Content-Type': 'application/json' }});
+    const response = thunk.dispatch(fetchBook());
+    return response;
+  } catch (error) {
+    return error;
+  }
+});
+
+export const removesBook = createAsyncThunk('books/removesBook', async (id, thunk) => {
+  try {
+    await axios.delete(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/6PniCavOBYZV6AbPZ91l/books/${id}`);
+    const response = thunk.dispatch(fetchBook());
+    return response;
+  } catch (error) {
+    return error;
+  }
+});
 
 const booksSlice = createSlice({
   name: 'books',
