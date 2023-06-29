aff41a5d52000370d17ea707535c690fea3841bd
forLoop94 <charlesuwem94@gmail.com>
2023-06-29 13:03:58 +0100

    Clear eslint errors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,19 @@
 import { Link } from 'react-router-dom';
 import styles from './styles/app.module.css';
 
-const Navbar = () => {
-  return (
-    <>
-      <nav>
-        <ul className={styles.navLinks}>
-          <li>
-            <Link className={styles.links} to="/">BOOKS</Link>
-          </li>
-          <li>
-            <Link className="links" to="/categories">CATEGORIES</Link>
-          </li>
-        </ul>
-      </nav>
-    </>
-  );
-};
+const Navbar = () => (
+  <>
+    <nav>
+      <ul className={styles.navLinks}>
+        <li>
+          <Link className={styles.links} to="/">BOOKS</Link>
+        </li>
+        <li>
+          <Link className="links" to="/categories">CATEGORIES</Link>
+        </li>
+      </ul>
+    </nav>
+  </>
+);
 
 export default Navbar;
